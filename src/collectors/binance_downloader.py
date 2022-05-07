5f322ec541bd5bfc8f6ecb051170c66bf1903b59
Tamim Azmain <tamim.azmain@rooter.co.jp>
2022-05-07 04:16:38 +0100

    upload to storage bucket instead

--- src/collectors/binance_downloader.py
+++ src/collectors/binance_downloader.py
@@ -1,18 +1,20 @@
 import requests
-import os
-from pathlib import Path
 from peewee import *
 import shutup
 from datetime import datetime
 import uuid
-
-
+import configparser
 from typing import *
+import boto3
+import tempfile
+from zipfile import ZipFile
+import io
+import re
 
 data_dump_root = "../../dumps"
 db = SqliteDatabase(f"{data_dump_root}/binance.db")
-
 shutup.please()
+
 
 class Prefix(Model):
     uuid = CharField()
@@ -78,29 +80,45 @@ class CrawlLog(Model):
 
 db.connect()
 
+
+config = configparser.ConfigParser()
+config.read("./config.ini")
+
+crawldata_config = {
+    "aws_access_key_id": config["BUCKETS"]["CrawlDataAccessKey"],
+    "aws_secret_access_key": config["BUCKETS"]["CrawlDataAccessSecret"],
+    "endpoint_url": config["BUCKETS"]["EndpointUrl"]
+}
+
+client = boto3.client("s3", **crawldata_config)
+
 query = (Key.select(Key.path, Key.name))
 
-for key in query:
+def extract_zip(input_zip) -> bytes:
+    input_zip=ZipFile(io.BytesIO(input_zip))
+    return input_zip.read(input_zip.namelist()[0])
 
-    # Example url
-    # https://data.binance.vision/data/futures/um/daily/klines/1000BTTCUSDT/12h/1000BTTCUSDT-12h-2022-04-11.zip
-
+def download_csv(key:Key):
     url = f"https://data.binance.vision/{key.path}"
-    print(url)
-    response = requests.get(url, stream = True)
+    response = requests.get(url, stream=True)
+    extracted_bytes = extract_zip(response.content)
+    return extracted_bytes.decode("utf-8", "ignore")
 
-    local_folder_path = data_dump_root + "/downloads/" + "/".join(key.path.split("/")[:-2])
-    print(local_folder_path)
-    Path(local_folder_path).mkdir(parents=True, exist_ok=True)
+for key in query:
+    if key.path.endswith("CHECKSUM"):
+        continue
 
-    local_file_path = local_folder_path + "/" + key.name
-    print(local_file_path)
-    
-    with open(local_file_path, "wb+") as f:
-        for chunk in response.iter_content(chunk_size=512):
-            if chunk:
-                f.write(chunk)
+    csv = download_csv(key)
+    with tempfile.NamedTemporaryFile("w+", encoding="utf-8") as fp:
+        fp.write(csv)
 
+        bucket_file_path = "/".join(key.path.split("/")[:-1])
+        bucket_file_name = re.sub(".zip$", ".csv", key.path.split("/")[-1])
 
-
+        file_key = bucket_file_path + "/" + bucket_file_name
+        client.upload_file(
+            Filename=fp.name,
+            Bucket='crawldata',
+            Key=file_key
+        )
 
